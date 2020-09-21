59a467128d270e04caa24151915707685a5779d1
askdfjlas <kaihengd@gmail.com>
2020-09-21 16:51:52 -0400

    DDB table

--- lib/cp-notes-lambda-stack.ts
+++ lib/cp-notes-lambda-stack.ts
@@ -1,5 +1,6 @@
 import * as cdk from '@aws-cdk/core';
 const apigateway = require('@aws-cdk/aws-apigateway');
+const dynamodb = require('@aws-cdk/aws-dynamodb');
 
 export class CpNotesLambdaStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -7,5 +8,10 @@ export class CpNotesLambdaStack extends cdk.Stack {
 
     const api = new apigateway.RestApi(this, 'cpNotes');
     api.root.addMethod('ANY');
+
+    const problems = new dynamodb.Table(this, 'problems', {
+      partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
+      sortKey: { name: 'sk', type: dynamodb.AttributeType.STRING }
+    });
   }
 }
