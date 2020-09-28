af54953da7ad9baca2af3de7c757b11d0c416e2d
askdfjlas <kaihengd@gmail.com>
2020-09-27 23:23:05 -0400

    Cognito user pool with email-verified login

--- lib/cp-notes-lambda-stack.ts
+++ lib/cp-notes-lambda-stack.ts
@@ -2,6 +2,7 @@ import * as cdk from '@aws-cdk/core';
 const apigateway = require('@aws-cdk/aws-apigateway');
 const dynamodb = require('@aws-cdk/aws-dynamodb');
 const lambda = require('@aws-cdk/aws-lambda');
+const cognito = require('@aws-cdk/aws-cognito');
 
 export class CpNotesLambdaStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -37,5 +38,34 @@ export class CpNotesLambdaStack extends cdk.Stack {
     const getProblemIntegration = new apigateway.LambdaIntegration(getProblemLambda);
 
     problemsResource.addMethod('GET', getProblemIntegration);
+
+    const userpool = new cognito.UserPool(this, 'cp-notes-users', {
+      userPoolName: 'cp-notes-users',
+      selfSignUpEnabled: true,
+      userVerification: {
+        emailSubject: 'Verify your email for cp-notes!',
+        emailBody: 'Hello there! Your verification code for cp-notes is {####}.',
+        emailStyle: cognito.VerificationEmailStyle.CODE
+      },
+      passwordPolicy: {
+        minLength: 6
+      },
+      autoVerify: { email: true },
+      signInAliases: {
+        username: true,
+        email: true
+      },
+      accountRecovery: cognito.AccountRecovery.EMAIL_ONLY
+    });
+
+    userpool.addClient('cp-notes-client', {
+      userPoolClientName: 'cp-notes-client',
+      generateSecret: false,
+      authFlows: {
+        userPassword: true,
+        userSrp: true,
+        refreshToken: true
+      }
+    });
   }
 }
